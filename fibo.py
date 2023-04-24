732f8068fe9528f3dbff527f79c35d65657ffc1d
Helena Henzlikova <helena.henzlikova@volny.cz>
2023-04-24 09:34:20 +0200

    fibo added

--- fibo.py
+++ fibo.py
@@ -1,7 +1,14 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
-    pass
-
+    n = 2
+    print(recursive_nth_fibo(n))
 
 if __name__ == "__main__":
     main()
